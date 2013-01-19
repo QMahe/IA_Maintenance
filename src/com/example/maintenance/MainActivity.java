beb124fc9e81b131c914c941a85b281a9cd16347
QMahe <quentin.mahe@hotmail.fr>
2013-01-20 00:46:01 +0100

    Commit de test en cas de modification

--- src/com/example/maintenance/MainActivity.java
+++ src/com/example/maintenance/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.maintenance;
 
+import java.text.SimpleDateFormat;
+import java.util.GregorianCalendar;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +14,12 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		// RECUPERER LA DATE
+		GregorianCalendar gc = new GregorianCalendar();
+		SimpleDateFormat dateFormat1 = new SimpleDateFormat("EEEE dd M");
+		String dateString = dateFormat1.format(gc.getTime());
+		Toast.makeText(MainActivity.this, dateString, Toast.LENGTH_SHORT).show();
 	}
 
 	@Override
